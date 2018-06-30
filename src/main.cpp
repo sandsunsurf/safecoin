63672840c781d10f13190565649f1e004fdb97be
FairExchange <fair-exchange@protonmail.com>
2018-06-30 16:06:43 +0000

    remove notary check

--- src/main.cpp
+++ src/main.cpp
@@ -2900,7 +2900,7 @@ bool ConnectBlock(const CBlock& block, CValidationState& state, CBlockIndex* pin
     }
     if ( block.vtx[0].GetValueOut() > blockReward+1 && pindex->nHeight > 103820)
     {
-        if ( ASSETCHAINS_SYMBOL[0] != 0 || pindex->nHeight >= SAFECOIN_NOTARIES_HEIGHT1 || block.vtx[0].vout[0].nValue > blockReward )
+        if ( ASSETCHAINS_SYMBOL[0] != 0 || pindex->nHeight >= 103820 || block.vtx[0].vout[0].nValue > blockReward )
         {
             return state.DoS(100,
                              error("ConnectBlock(): coinbase pays too much (actual=%d vs limit=%d)",
