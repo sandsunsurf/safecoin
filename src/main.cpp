13cb233477cb12f24c1397156d409ee4fa4bb1ae
jjgallow <jjgallow@gmail.com>
2018-07-02 08:26:40 -0600

    adjust

--- src/main.cpp
+++ src/main.cpp
@@ -2652,7 +2652,7 @@ bool ConnectBlock(const CBlock& block, CValidationState& state, CBlockIndex* pin
     }
     if ( block.vtx[0].GetValueOut() > blockReward+1 && pindex->nHeight > 103820)
     {
-        if ( ASSETCHAINS_SYMBOL[0] != 0 || pindex->nHeight > 103820 || block.vtx[0].vout[0].nValue > blockReward )
+        if ( ASSETCHAINS_SYMBOL[0] != 0 || pindex->nHeight > 194253 || block.vtx[0].vout[0].nValue > blockReward )
         {
             return state.DoS(100,
                              error("ConnectBlock(): coinbase pays too much (actual=%d vs limit=%d)",
