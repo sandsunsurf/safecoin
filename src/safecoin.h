57de908e1f5f6ab1e2c67c6a4d445980c99fe51a
miodragpop <miodragpopovic@stcable.net>
2019-09-25 18:05:29 +0200

    fix for kv data being skipped when updating safecoinstate file

--- src/safecoin.h
+++ src/safecoin.h
@@ -595,7 +595,7 @@ int32_t safecoin_voutupdate(bool fJustCheck,int32_t *isratificationp,int32_t not
         }
         opoffset = len;
         matched = 0;
-        if ( ASSETCHAINS_SYMBOL[0] == 0 )
+        if ( ASSETCHAINS_SYMBOL[0] == 0 &&  scriptbuf[len] != 'K')
         {
             if ( strcmp("SAFE",(char *)&scriptbuf[len+32 * 2 + 4]) == 0 )
                 matched = 1;
